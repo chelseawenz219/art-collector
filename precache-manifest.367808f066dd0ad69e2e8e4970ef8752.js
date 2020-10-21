7de169a3cb90f8bc921593a5aae224f0de14aa64
Chelsea <cw.19courtney@gmail.com>
2020-10-21 18:41:11 -0400

    Updates

---
+++ precache-manifest.367808f066dd0ad69e2e8e4970ef8752.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5754500d034feebab0364516bed4d4a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8adb3a1cab9e6012fcc7",
+    "url": "/static/js/2.6c99d600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c99d600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da27ef0aadd3ada6a5db",
+    "url": "/static/js/main.a48d7976.chunk.js"
+  },
+  {
+    "revision": "aa7c612222590812d412",
+    "url": "/static/js/runtime-main.1160daa0.js"
+  }
+]);
